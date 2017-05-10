21de2cccbf4f42f84b9b2cd8c333d8010b985979
Sarah Colborne <skcolborne@gmail.com>
2017-05-10 20:41:44 -0300

    Added assignment 1 changes, button functionality with text

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText textField;
+        textField = (EditText) findViewById(R.id.editText);
+
+        final TextView textBox;
+        textBox = (TextView) findViewById(R.id.textView);
+
+        Button button;
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View x){
+                String temp = textField.getText().toString(); //get text from textField when button is clicked
+                textBox.setText(temp); //insert text into textBox
+            }
+
+        });
+
+
     }
 }
